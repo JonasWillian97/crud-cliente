6a109d1b554ab6eaa75f566eec48f30bb79e0a89
Jonas Willian <jwnarancity@gmail.com>
2023-10-27 13:36:08 -0300

    Criação da camada de services

---
+++ src/app/services/clientes.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/Cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  private url: string = "http://localhost:3000/clientes";
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   getAll(cliente: Cliente):Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+   }
+}
